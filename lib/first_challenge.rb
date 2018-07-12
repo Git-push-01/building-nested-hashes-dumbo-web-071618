af07d9f44843e20af0d2180cd9920cb4ab148d04
Hugo Escandon <hescandon6@gmail.com>
2018-07-12 14:58:30 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -3,7 +3,7 @@ def first_challenge
      :montague => {
        :patriarch => {:name => "Lord Montague", :age => "53"},
        :matriarch => {:name => "Lady Montague", :age => "54"},
-       :hero => {:name => "Romeo", :age => "15", :status => "alive"},
+       :hero => {:name => "Romeo", :age => "15", :status => "dead"},
        :hero_friends => [{
          :name => "Benvolio",
          :age => "17",
@@ -20,7 +20,7 @@ def first_challenge
      :capulet => {
        :patriarch => {:name => "Lord Capulet", :age => "50"},
        :matriarch => {:name => "Lady Capulet", :age => "51"},
-       :heroine => {:name => "Juliet", :age => "15", :status => "alive"},
+       :heroine => {:name => "Juliet", :age => "15", :status => "dead"},
        :heroine_friends => [{
         :name => "Steven",
         :age => "30",
